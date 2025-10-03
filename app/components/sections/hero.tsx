4769eb13c97f57f99001e6e2e8957d3e5652cef7
Love Akinlesi <akinlesil@gmail.com>
2025-10-04 00:41:35 +0100

    feat: enhance hero section with text fade animation and add TextFade component

--- app/components/sections/hero.tsx
+++ app/components/sections/hero.tsx
@@ -1,15 +1,19 @@
 import { useGSAP } from "@gsap/react";
 import { gsap } from "gsap";
 import ScrollTrigger from "gsap/dist/ScrollTrigger";
+import SplitText from "gsap/dist/SplitText";
 import { useRef } from "react";
 import { Button } from "~/components/ui/button";
 import { Link } from "~/components/ui/link";
+import { TextFade } from "../ui/text-fade-up";
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
 
 export default function Hero() {
   const sectionRef = useRef<HTMLDivElement | null>(null);
   const imgRef = useRef<HTMLImageElement | null>(null);
+  const h1Ref = useRef<HTMLHeadingElement | null>(null);
 
   useGSAP(() => {
     const ctx = gsap.context(() => {
@@ -34,6 +38,18 @@ export default function Hero() {
           },
         }
       );
+
+      const mySplitText = new SplitText("h1", { type: "words,chars,lines", linesClass:"overflow-hidden leading-[1.15]",}, );
+      const chars = mySplitText.chars;
+
+      gsap.fromTo(mySplitText.lines, {
+        yPercent: 100,
+        opacity: 0,
+        stagger: 0.02,
+        ease: "back.out",
+        duration: 0.7,
+      }, { yPercent: 0, opacity: 1, stagger: 0.2, ease: "back.out", duration: 0.7});
+
     }, sectionRef);
 
     return () => ctx.revert();
@@ -63,13 +79,12 @@ export default function Hero() {
 
       <section className="flex flex-row justify-between relative grow-1 shrink-1 basis-0 section my-12 md:my-20 py-10">
         <div className="md:max-w-14/20 w-full gap-4 md:gap-6 mx-0 md:min-h-50 h-auto flex flex-col lg:grid content-center justify-center md:justify-start ">
-          <div className="flex flex-col gap-6 items-start text-[#f4f4f4]">
+          <TextFade direction="down" className="flex flex-col gap-6 items-start text-[#f4f4f4]">
             <h1 className="text-[clamp(2rem,_1.6rem+1.64vw,_2.5rem)] md:text-[clamp(3rem,_1.8rem_+_2.18667vw,_5rem)] font-semibold tracking-[-0.075rem] whitespace-pre-line  leading-none text-left">
             Greatness from Creative Beginnings, Powering Global Brands 
             </h1>
             <p className="text-base md:text-lg text-left">
               Accelra is a private equity and credit firm investing in the creative sectors. We deploy capital and strategic support to transform culture, lifestyle, and talent into world-class brands that shape Africa and Europe’s future.</p>
-          </div>
 
           <div className="flex justify-start gap-4">
             {/* <FormModal formId="3xKJ0J" trigger={} /> */}
@@ -80,6 +95,7 @@ export default function Hero() {
               <Button variant="default" className="text-white bg-white/10">Pitch Us Your Business</Button>
             </Link>
           </div>
+          </TextFade>
         </div>
       </section>
     </section>
